c1ca5276a873a5eda658602b96b089c423efd00a
Vasil Chirov <vasilchirov@gmail.com>
2026-06-01 13:46:08 +0200

    Revert "fixed cache dir name for regression finetune"
    This reverts commit 6b0f16ad0ddf151f660d85c45a3f1d37e624d30d.

--- src/finetune-regression.py
+++ src/finetune-regression.py
@@ -28,8 +28,8 @@ except ModuleNotFoundError:
 @dataclass
 class FinetuneConfig:
     model_name: str = "roberta-base"
-    cache_dir: str = "./argument_model-roberta-regression"
-    output_dir: str = "./results/finetune-roberta-regression"
+    cache_dir: str = "./argument_model-roberta"
+    output_dir: str = "./results/finetune-roberta"
     max_length: int = 512
     learning_rate: float = 2e-5
     per_device_train_batch_size: int = 8
